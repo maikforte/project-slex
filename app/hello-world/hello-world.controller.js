9cdd25bab814cacbe3722be221553b3ef13a77d5
Michael Ardan <michael_ardan2000@yahoo.com>
2020-05-15 18:43:04 +0800

    bumps

--- app/hello-world/hello-world.controller.js
+++ app/hello-world/hello-world.controller.js
@@ -1,4 +1,17 @@
-module.exports = () => {
+module.exports = (axios) => {
+    const request = (type, params, body) => axios({
+        headers: {
+            'Authorization': `Bearer ${API_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        url: `${BASE_URL}${ENDPOINT}`,
+        method: type,
+        data: body,
+        params,
+    });
+
+    const requestUsage = async params => request('get', params);
+
     const getHelloWorld = () => 'Hello World';
 
     const sayHello = name => `Hello ${name}`;
--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 // Dependencies
 const app = require('express')();
 
+// Environment
+require('dotenv').config();
+
 // Utilities
 const config = require('./config/config');
+
+// Middlewares
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 // App Components
 const helloWorld = require('./app/hello-world')();
 
+// Routes
 app.use(helloWorld);
 
 app.listen(config.port, () => {
